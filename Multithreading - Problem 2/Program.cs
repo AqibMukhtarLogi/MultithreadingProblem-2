7a78590960cd7af4aa490f9f54e568096ad3d931
Aqib Mukhtar <aqib.mukhtar@logiciel.com>
2021-09-09 23:28:11 -0700

    Implementation 1

---
+++ Multithreading - Problem 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace Multithreading___Problem_2
+{
+    class Program
+    {
+        static void Print(Object obj)
+        {
+            for(int i = (int) obj; i <= 100; i += 10)
+                Console.WriteLine("Thread: {0}, Number: {1}", Thread.CurrentThread.GetHashCode(), i);
+        }
+        static void Main(string[] args)
+        {
+            List<Thread> threads = new List<Thread>();
+            for (int i = 0; i < 10; i++)
+            {
+                threads.Add(new Thread(new ParameterizedThreadStart(Print)));
+                threads[i].Start(i + 1);
+            }
+
+        }
+    }
+}
